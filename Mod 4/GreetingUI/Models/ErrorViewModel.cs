640c851f2ea64f86cf4c994fd48bad4f74261481
Chris Schmidlin <chris.schmidlin@gmail.com>
2019-09-19 21:42:03 -0500

    Final commit to this folder

---
+++ Mod 4/GreetingUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GreetingUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mod 5/mod5lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mod5lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
